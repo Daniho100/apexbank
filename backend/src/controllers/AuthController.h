9f8f13f2672206d15fb611acae329e708bccc700
Daniho100 <daniandre.dah@gmail.com>
2026-07-10 20:41:42 +0100

    used absolute routes with caret symbol in AuthController to bypass default prefix

--- backend/src/controllers/AuthController.h
+++ backend/src/controllers/AuthController.h
@@ -5,14 +5,9 @@ namespace banking::controllers {
 
 class AuthController : public drogon::HttpController<AuthController> {
 public:
-    static const std::vector<std::string> &paths() {
-        static const std::vector<std::string> _paths = { "/api/auth" };
-        return _paths;
-    }
-
     METHOD_LIST_BEGIN
-    METHOD_ADD(AuthController::registerUser, "/register", drogon::Post);
-    METHOD_ADD(AuthController::loginUser, "/login", drogon::Post);
+    METHOD_ADD(AuthController::registerUser, "^/api/auth/register", drogon::Post);
+    METHOD_ADD(AuthController::loginUser, "^/api/auth/login", drogon::Post);
     METHOD_LIST_END
 
     void registerUser(const drogon::HttpRequestPtr& req, std::function<void(const drogon::HttpResponsePtr&)>&& callback);
