847a5cdae957f8a06411447a90449a7de7d2e58e
Daniho100 <daniandre.dah@gmail.com>
2026-07-10 21:03:57 +0100

    used ADD_METHOD_TO for absolute route mapping

--- backend/src/controllers/AuthController.h
+++ backend/src/controllers/AuthController.h
@@ -6,8 +6,8 @@ namespace banking::controllers {
 class AuthController : public drogon::HttpController<AuthController> {
 public:
     METHOD_LIST_BEGIN
-    METHOD_ADD(AuthController::registerUser, "^/api/auth/register", drogon::Post);
-    METHOD_ADD(AuthController::loginUser, "^/api/auth/login", drogon::Post);
+    ADD_METHOD_TO(AuthController::registerUser, "/api/auth/register", drogon::Post);
+    ADD_METHOD_TO(AuthController::loginUser, "/api/auth/login", drogon::Post);
     METHOD_LIST_END
 
     void registerUser(const drogon::HttpRequestPtr& req, std::function<void(const drogon::HttpResponsePtr&)>&& callback);
